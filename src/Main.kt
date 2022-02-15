21fa60792263893e86780f028e2fe08d5a6a50a6
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-02-15 22:48:47 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var name: String = "Shaline Jerun"
+    println(name)
+    var age = 23
+    println(age)
+    var phoneNumber: String ="0707234567"
+     println(phoneNumber)
+    var weight:Float =57f
+    println(weight)
+    val citizen:Boolean =true
+      println(citizen)
+}
+
+
+
+
+
+
+
