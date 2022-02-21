4b448c8b1e3e239352707172fccac2d6c8c5d43c
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-02-21 06:33:26 +0300

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,32 @@
-fun main() {
-    var name: String = "Shaline Jerun"
-    println(name)
-    var age = 23
-    println(age)
-    var phoneNumber: String ="0707234567"
-     println(phoneNumber)
-    var weight:Float =57f
-    println(weight)
-    val citizen:Boolean =true
-      println(citizen)
+fun main(){
+    var sayHelloSylviah=("Hello Sylviah")
+    println(sayHelloSylviah)
+    modulus(27,5)
+    var total = Sum(9, 7, 6, 5)
+    println(total)
+    printFact("Singing")
+}
+
+fun sayHelloSylviah(Name:String){
+    println("Hello" + Name)
+
+
+}
+
+fun modulus(num1:Int, num2:Int): Int{
+    var x = (num1 % num2)
+    println(x)
+    return x
 }
 
 
+   fun Sum(a: Int,b:Int,c:Int,d:Int):Int{
+      var sum = a + b + c + d
+    return sum
+}
 
-
-
+fun printFact(fact:String){
+    println("I like $fact")
+}
 
 
